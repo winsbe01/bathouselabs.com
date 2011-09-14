0f348326f882262d5e09941a67febf99017f0ed9
Benjamin Winston <winsbe01@gmail.com>
2011-09-14 18:50:30 -0400

    edited email script, published radiohead review

---
+++ email.markdown
@@ -0,0 +1,12 @@
+---
+---
+Hello!
+        
+Just wanted to let you know, [something new][1] has just been published on bathouselabs.com. Hope you enjoy it!
+
+Yours,<br />
+Ben
+
+{% for post in site.posts limit: 1 %}
+[1]: http://bathouselabs.com{{ post.url }}
+{% endfor %}
--- email.sh
+++ email.sh
@@ -2,5 +2,5 @@
 
 while read line
 do
-    mail -s "an update from bathouselabs.com" -a "Content-type: text/html; charset=iso-8859-1\nFrom: Ben Winston <benjamin@bathouselabs.com>" $line < email.html
+    mail -s "an update from bathouselabs.com" -a "Content-type: text/html; charset=iso-8859-1\nReply-To: Ben Winston <benjamin@bathouselabs.com>\nFrom: Ben Winston <benjamin@bathouselabs.com>" $line < _site/email.html
 done < _site/misc/emails.txt
